798658fbaa0cc27baf18764705bb212c00db45d5
Anshuman <ansh9010@gmail.com>
2020-06-12 09:36:42 +0530

    update base.html and settings.py

--- web_scrapper_project/settings.py
+++ web_scrapper_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*7^nm8h^lp$pjy%j3lry*hydr4p(1wph2ij27k26+87j6ple92'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['mynewscrapperapp.herokuapp.com']
+ALLOWED_HOSTS = ['mynewscrapperapp.herokuapp.com','127.0.0.1']
 
 
 # Application definition
