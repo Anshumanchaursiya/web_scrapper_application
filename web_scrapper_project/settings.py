3da2a9ce67dcff62f5a23b29b7cf80484d472adb
Anshuman <ansh9010@gmail.com>
2020-06-14 16:11:49 +0530

    add css

--- web_scrapper_project/settings.py
+++ web_scrapper_project/settings.py
@@ -25,6 +25,7 @@ SECRET_KEY = '*7^nm8h^lp$pjy%j3lry*hydr4p(1wph2ij27k26+87j6ple92'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+
 ALLOWED_HOSTS = ['mynewscrapperapp.herokuapp.com','127.0.0.1']
 
 
@@ -117,7 +118,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static')
+]
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 
